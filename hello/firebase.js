697b074f0604065d845120ed3a163579916eaa5c
Hamzabaloch08 <hb.developer08@gmail.com>
2025-02-25 22:50:36 +0500

    add firebase sdk

---
+++ hello/firebase.js
@@ -0,0 +1,24 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.3.1/firebase-app.js";
+import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/11.3.1/firebase-auth.js";
+
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyD3apjMA0JnH2gNOQolZP0GQAR_G4GxOPM",
+    authDomain: "fir-learning-e1aba.firebaseapp.com",
+    projectId: "fir-learning-e1aba",
+    storageBucket: "fir-learning-e1aba.appspot.com",
+    messagingSenderId: "627452201110",
+    appId: "1:627452201110:web:2b605133c37b46b82a5ab7"
+
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+export {
+    auth,
+    createUserWithEmailAndPassword
+};
